26232c7762d930c152040e5cc22170d0e080eab3
Nino <ntorres-ulisses@turgot-paris.info>
2022-11-24 10:53:15 +0100

    initialisation

---
+++ vues/v_listeValiderFrais.php
@@ -0,0 +1,65 @@
+<?php if($etat) { ?>
+<div class="corpsform">
+    <h3>Fiche de frais du mois <?php echo $numMois . "-" . $numAnnee ?> :
+    </h3>
+    <div class="encadre">
+        <p>Etat : <?= $etat["libelle"] ?> depuis le <?= $date_fiche ?> <br> Montant validé : <?= $etat["montant"] ?></p>
+        <table class="listeLegere">
+            <caption>Eléments forfaitisés </caption>
+            <tr>
+                <?php
+                foreach ($lesFraisForfait as $unFraisForfait) {
+                    $libelle = $unFraisForfait['libelle'];
+                ?>
+                    <th> <?php echo $libelle ?></th>
+                <?php
+                }
+                ?>
+            </tr>
+            <tr>
+                <?php
+                foreach ($lesFraisForfait as $unFraisForfait) {
+                    $quantite = $unFraisForfait['quantite'];
+                ?>
+                    <td class="qteForfait"><?php echo $quantite ?> </td>
+                <?php
+                }
+                ?>
+            </tr>
+        </table>
+        <div class="piedForm">
+            <p>
+                <input id="ok" type="submit" value="Valider" size="20" />
+                <input id="annuler" type="reset" value="Effacer" size="20" />
+            </p>
+        </div>
+        <table class="listeLegere">
+            <caption>Descriptif des éléments hors forfait -<?php echo $nbJustificatifs ?> justificatifs reçus -
+            </caption>
+            <tr>
+                <th class="date">Date</th>
+                <th class="libelle">Libellé</th>
+                <th class='montant'>Montant</th>
+                <th colspan="2"  class='action'>Action</th>
+            </tr>
+            <?php
+            
+            foreach ($lesFraisHorsForfait as $unFraisHorsForfait) {
+                $date = $unFraisHorsForfait['date'];
+                $libelle = $unFraisHorsForfait['libelle'];
+                $montant = $unFraisHorsForfait['montant'];
+            ?>
+                <tr>
+                    <td><?php echo $date ?></td>
+                    <td><?php echo $libelle ?></td>
+                    <td><?php echo $montant ?></td>
+                    <td><a href="index.php?uc=gererFrais&action=supprimerFrais&idFrais=<?= $unFraisHorsForfait["id"]?>" title="Supprimer">Supprimer</a></td>
+                    <td><a href="index.php?uc=gererFrais&action=supprimerFrais" title="Reporter">Reporter</a></td>
+                </tr>
+            <?php
+            }
+            ?>
+        </table>
+    </div>
+</div>
+<?php } ?>
